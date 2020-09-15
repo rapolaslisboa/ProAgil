6815c2059d698d23d842d11b528df4e526998b1e
Rápolas Lisboa <rapolas.lisboa@gmail.com>
2020-09-15 16:31:01 +0200

    Criação da model Evento, definição do seu controller e criação de um data context

---
+++ ProAgil/ProAgil/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProAgil.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProAgil.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Evento> Eventos { get; set; }
+    }
+}
